4c01649a08cebbee681c52e879fcb54868908954
WiinBit <peterblagrove@icloud.com>
2024-04-30 15:01:04 -0600

    Explicitly add all required dependincies cause wth...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// List all the dependencies from your node_modules folder
+const dependencies = [
+  '@remix-run',
+  'asynckit',
+  'axios',
+  'combined-stream',
+  'delayed-stream',
+  'follow-redirects',
+  'form-data',
+  'js-tokens',
+  'jwt-decode',
+  'loose-envify',
+  'mime-db',
+  'mime-types',
+  'proxy-from-env',
+  'react',
+  'react-dom',
+  'react-router',
+  'react-router-dom',
+  'scheduler'
+];
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['jwt-decode', 'react-router-dom']
+      external: dependencies
     }
   }
-})
+});
