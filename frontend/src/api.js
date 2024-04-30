0881987aae005d7574570549babdf7fef5a6a510
WiinBit <peterblagrove@icloud.com>
2024-04-30 14:13:18 -0600

    apiUrl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./contstants"
 
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
